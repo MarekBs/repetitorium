6b8418f258945b73d3d0ea6e04be3a38482c81d5
marekBs <marek.belis810@gmail.com>
2025-09-27 22:09:05 +0200

    change deploy settings to medTest2

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
-const basePath = process.env.NODE_ENV === "production" ? "/medTest/" : "/";
+const basePath = process.env.NODE_ENV === "production" ? "/medTest2/" : "/";
 
 export default defineConfig({
   plugins: [
