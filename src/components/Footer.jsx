2a9f6f006cdb5997dcf086c4d9b5e39e86d89def
marekBs <marek.belis810@gmail.com>
2025-09-29 10:23:52 +0200

    fix white line bottom

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from "react";
 
 export default function Footer() {
   return (
-    <div className='ftr'>
-        <div className='imgHolder'><img src={`${import.meta.env.BASE_URL}logoMED.svg`} alt="logoMED" className="logoIMG" height={100}/></div>
-    <p className='credentials'>©MB 2024/25.</p>
+    <div className="ftr">
+      <img
+        src={`${import.meta.env.BASE_URL}logoMED.svg`}
+        alt="logoMED"
+        className="logoIMG"
+        height={100}
+      />
+      <p className="credentials">©MB 2024/25.</p>
     </div>
-  )
+  );
 }
